ca6b075ba7cab9cf48188a5fbf1a8d035340faa9
Carolyn B <bmathematics@gmail.com>
2022-02-02 11:35:20 -0500

    added errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,34 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitedWarningEl = document.querySelector("#limit-warning");
+// var queryString = document.location.search;
+var repoNameEl = document.querySelector("#repo-name");
+
+var displayWarning = function(repo){
+    //add text to warning container
+    limitedWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitedWarningEl.appendChild(linkEl);
+};
+
+var getRepoName = function(){
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName){
+        getRepoIssues(repoName);
+        repoName.textContent = repoName;
+    }
+    else{
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function(repo){
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,10 +38,16 @@ var getRepoIssues = function(repo){
            response.json().then(function(data){
                //pass response data to dom function
                displayIssues(data);
+
+               //check if api has paginated issues
+               if (response.headers.get("Link")){
+                   displayWarning(repo);
+               }
            });
        }
        else{
-           alert("There was a problem with your request!");
+           //if not successful, redirect to homepage
+        document.location.replace("./index.html");
        }
    });
 };
@@ -52,5 +88,5 @@ var displayIssues = function(issues){
         issueEl.appendChild(typeEl);
     }
 };
-getRepoIssues("facebook/react");
+getRepoName();
     
